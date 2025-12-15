2dc10324abf4db3005f3be73c4114d2c167b56e8
Bifos-pan <m8kbnymd5y@privaterelay.appleid.com>
2025-12-15 23:16:17 +0300

    Add files via upload

---
+++ Новая папка (2)/задача 1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+	double l, ro, s, v, cl;
+	cout << "Введите параметры для рассчета полета" << endl;
+	cout << "Площадь крыла самолета ";
+	cin >> s;
+	cout << "Скорость полета ";
+	cin >> v;
+	cout << "Плотность воздуха ";
+	cin >> ro;
+	cout << "Коэфициент подъемной силы ";
+	cin >> cl;
+	l = 0.5 * ro * pow(v, 2) * s * cl;
+	cout << "Подъемная сила равна " << l;
+	return 0;
+}
